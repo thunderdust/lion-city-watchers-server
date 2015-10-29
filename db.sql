a6fa7c2235325cb8af6a945bf79da6d625dceebf
thunderdust <liuweiran.nus@gmail.com>
2015-10-29 15:49:24 +0800

    Add database table creation queries

---
+++ db.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `user` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`username` VARCHAR(50) NOT NULL,
+	`email` VARCHAR(50) NOT NULL,
+	`password` VARCHAR(50) NOT NULL,
+	`last_login` DATETIME NULL DEFAULT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE INDEX `email` (`email`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB;
+
+CREATE TABLE `report` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`description` VARCHAR(256) NOT NULL,
+	`reported_by` INT(11) NOT NULL,
+	`reported_at` DATETIME NOT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB;
+
