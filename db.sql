a4f89a5d7bc726ffb9ce3334b0ef2843e49a1442
thunderdust <liuweiran.nus@gmail.com>
2015-11-01 18:26:24 +0800

    Update database creation schema

--- db.sql
+++ db.sql
@@ -2,13 +2,14 @@ CREATE TABLE `users` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`username` VARCHAR(50) NOT NULL,
 	`email` VARCHAR(50) NOT NULL,
-	`password` VARCHAR(50) NOT NULL,
+	`password` VARCHAR(256) NOT NULL,
 	`last_login` DATETIME NULL DEFAULT NULL,
 	PRIMARY KEY (`id`),
 	UNIQUE INDEX `email` (`email`)
 )
 COLLATE='utf8_general_ci'
-ENGINE=InnoDB;
+ENGINE=InnoDB
+AUTO_INCREMENT=9;
 
 CREATE TABLE `reports` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
